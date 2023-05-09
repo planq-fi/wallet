9f82d39697b81454c7d8611a97c568f8644715f8
Ivan Shukhov <45999338+ivanshukhov@users.noreply.github.com>
2023-05-09 17:58:45 +0200

    remove mycrypto api calls

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -6,23 +6,18 @@ export const POCKET_API_KEY = '5f6a53d75053d3232349e41e';
 
 export const ETHERSCAN_API_KEY = '3BJCKMTC6BY9XEJPZEQ7BIWJR4MCP94UB4';
 
-export const MYC_API_MAINNET = 'https://api.mycryptoapi.com/eth';
-
 export const DEFI_RESERVE_MAPPING_URL = 'https://defi.mycryptoapi.com';
 
-export const GITHUB_RELEASE_NOTES_URL = 'https://github.com/MyCryptoHQ/MyCrypto/releases/latest' as TURL;
-
-// The URL for Token Info API requests.
-export const TOKEN_INFO_URL = 'https://api.mycryptoapi.com/tokens';
+export const GITHUB_RELEASE_NOTES_URL = 'https://github.com/planq-fi/wallet/releases/latest' as TURL;
 
 // The URL for MYC api.
-export const MYC_API = 'https://mycryptoapi.com/api/v1';
+export const MYC_API = 'https://localhost:3000';
 
 export const HISTORY_API = 'https://history.mycryptoapi.com/history';
 
 export const NANSEN_API = 'https://nansen.mycryptoapi.com/v2';
 
-export const CUSTOM_ASSET_API = 'https://assets.mycryptoapi.com/custom';
+export const CUSTOM_ASSET_API = '';
 
 export const ENS_MANAGER_URL = 'https://app.ens.domains';
 
@@ -37,7 +32,7 @@ export const OPENSEA_IMAGE_PROXY = 'https://mycryptoapi.com/api/v1/images/opense
 export const POAP_CLAIM_API = 'https://poap.mycryptoapi.com/v1';
 
 // The URL and site ID for the Matomo analytics instance.
-export const ANALYTICS_API = 'https://analytics.mycryptoapi.com';
+export const ANALYTICS_API = 'https://analytics.planq.finance';
 export const ANALYTICS_SITE_ID_PROD = 17;
 export const ANALYTICS_SITE_ID_DEV = 11;
 
@@ -58,7 +53,7 @@ export const MYC_DEX_COMMISSION_RATE = 0.0025;
 
 export const CREATION_ADDRESS = '0x0000000000000000000000000000000000000000';
 
-export const DEFAULT_NETWORK_CHAINID = 1;
+export const DEFAULT_NETWORK_CHAINID = 7070;
 
 export const SECONDS_IN_MONTH = 60 * 60 * 24 * 30;
 
@@ -78,8 +73,8 @@ export const ETH_SCAN_BATCH_SIZE = 300;
 export const DEFAULT_NUM_OF_ACCOUNTS_TO_SCAN = 5;
 export const DEFAULT_GAP_TO_SCAN_FOR = 5;
 
-export const SETTINGS_FILENAME = 'MyCrypto_Settings_File';
+export const SETTINGS_FILENAME = 'PlanqFi_Wallet_Settings_File';
 
 export const ASSET_DROPDOWN_SIZE_THRESHOLD = 150;
 
-export const PRIVACY_POLICY_LINK = 'https://mycrypto.com/privacy/';
+export const PRIVACY_POLICY_LINK = 'https://planq.network/privacy-policy/';
